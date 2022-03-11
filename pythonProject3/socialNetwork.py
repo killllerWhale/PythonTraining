95972bf04c5afca186a3c17bccbc9a79938261de
killllerWhale <93337303+killllerWhale@users.noreply.github.com>
2022-03-11 22:55:46 +0300

    практика 4

--- pythonProject3/socialNetwork.py
+++ pythonProject3/socialNetwork.py
@@ -3,10 +3,10 @@ from abc import ABC, abstractmethod
 class Social_Network(ABC):
 
     def message_user(self, user, message):
-        user.messages.append(message)
+        user.new_massages.append(message)
 
     def message_wall(self, message):
-        self.messages.append(message)
+        self.new_massages.append(message)
 
     @abstractmethod
     def dop_info(self):
@@ -16,11 +16,52 @@ class Social_Network(ABC):
         print("Имя пользователя: " + self.name+" ")
         self.dop_info()
 
-class user(Social_Network):
+    def get_massages(self):
+        while True:
+            if len(self.new_massages) > 0:
+                print("У вас: " + str(len(self.new_massages)) + " новых сообщений ")
+                print("Если хотите просмотреть новые сообщения введите '1' " )
+                print("Если хотите выйти введите 'end' ")
+                if len(self.massages) > 0:
+                    print("Если хотите просмотреть старые сообщения введите '2' ")
+                choice = input()
+                if choice == "1":
+                    for i in range(len(self.new_massages)):
+                        print(self.new_massages[i])
+                elif choice == "2":
+                    for i in range(len(self.massages)):
+                        print(self.massages[i])
+                elif choice == "end":
+                    self.massages.extend(self.new_massages)
+                    self.new_massages = []
+                    break
+                else:
+                    print("Вы ввели неверное значение, попробуйте еще раз! ")
+            else:
+                print("У вас нет новых сообщений! ")
+                if len(self.new_massages) > 0:
+                    print("Если хотите просмотреть старые сообщения введите '2' ")
+                    print("Если хотите выйти введите 'end' ")
+                choice = input()
+                if choice == "2":
+                    for i in range(len(self.massages)):
+                        print(self.massages[i])
+                elif choice == "end":
+                    self.massages.extend(self.new_massages)
+                    self.new_massages = []
+                    break
+                else:
+                    print("Вы ввели неверное значение, попробуйте еще раз! ")
+                    continue
+                print("У вас нет сообщений! ")
+                break
+
+class User(Social_Network):
 
     def __init__(self, name, date_of_birth):
         self.name = name
         self.date_birth = date_of_birth
+        self.new_massages = []
         self.massages = []
         self.user_subscriptions = []
 
@@ -38,7 +79,17 @@ class Community(Social_Network):
         self.name = name
         self.description = description
         self.massages = []
+        self.new_massages = []
         self.user_subscriptions = []
 
     def dop_info(self):
         print("Описание: " + str(self.description))
+
+
+
+a = User("Паша", "10.10.2003")
+b = User("Маша", "10.10.2003")
+a.message_user(b, "Привет")
+b.get_massages()
+a.message_user(b, "Пока")
+b.get_massages()
